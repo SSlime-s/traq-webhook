692c839a08c67b5329d911855a2312dfca156345
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-06-12 18:31:12 +0900

    🎨 add ** **

--- index.js
+++ index.js
@@ -13,54 +13,55 @@ const makeMessage = context => {
     const issue = payload.issue
     content = [
       `## :git_issue_opened: [${issue.title}](${issue.html_url})が作成されました`,
-      `リポジトリ: ${payload.repository.name}`,
-      `作成者: ${context.actor}`,
+      `**リポジトリ**: ${payload.repository.name}`,
+      `**作成者**: ${context.actor}`,
       ...(issue.body.length === 0 ? [] : ['', '---', issue.body])
     ].join('\n')
   } else if (context.eventName === 'issues' && payload.action === 'closed') {
     const issue = payload.issue
     content = [
       `## :git_issue_closed: [${issue.title}](${issue.html_url})が閉じられました`,
-      `リポジトリ: ${payload.repository.name}`,
-      `作成者: ${context.actor}`,
+      `**リポジトリ**: ${payload.repository.name}`,
+      `**作成者**: ${context.actor}`,
       ...(issue.body.length === 0 ? [] : ['', '---', issue.body])
     ].join('\n')
   } else if (context.eventName === 'issue_comment' && payload.action === 'created') {
     const issue = payload.issue
     content = [
       `## :comment: [${issue.title}](${issue.html_url}) にコメントが追加されました`,
-      `リポジトリ: ${payload.repository.name}`,
-      `コメントした人: ${context.actor}`
+      `**リポジトリ**: ${payload.repository.name}`,
+      `**コメントした人**: ${context.actor}`,
+      ...(payload.comment.body === 0 ? ['', '---', payload.comment.body] : [])
     ].join('\n')
   } else if (context.eventName === 'pull_request' && payload.action === 'opened') {
     const pr = payload.pull_request
     content = [
       `## :git_pull_request: [${pr.title}](${pr.html_url}) が作成されました`,
-      `リポジトリ: ${payload.repository.name}`,
-      `作成者: ${context.actor}`,
-      ...calcHMACSHA1(pr.body.length === 0 ? []: ['', '---', pr.body])
+      `**リポジトリ**: ${payload.repository.name}`,
+      `**作成者**: ${context.actor}`,
+      ...(pr.body.length === 0 ? []: ['', '---', pr.body])
     ].join('\n')
   } else if (context.eventName === 'pull_request' && payload.action === 'closed') {
     const pr = payload.pull_request
     content = [
       `## :git_pull_request_closed: [${pr.title}](${pr.html_url}) がマージされました :tada:`,
-      `リポジトリ: ${payload.repository.name}`,
-      `作成者: ${context.actor}`,
-      ...calcHMACSHA1(pr.body.length === 0 ? []: ['', '---', pr.body])
+      `**リポジトリ**: ${payload.repository.name}`,
+      `**作成者**: ${context.actor}`,
+      ...(pr.body.length === 0 ? []: ['', '---', pr.body])
     ].join('\n')
   } else if (context.eventName === 'pull_request' && payload.action === 'review_requested') {
     // TODO リクエストされた人をだす
     const pr = payload.pull_request
     content = [
       `## :eyes_wave: [${pr.title}](${pr.html_url}) でレビューがリクエストされました`,
-      `リポジトリ: ${context.actor}`
+      `**リポジトリ**: ${context.actor}`
     ].join('\n')
   } else if (context.eventName === 'pull_request_review' && payload.action === 'submmitted') {
     const pr = payload.pull_request
     content = [
       `## :blob_slide: [${pr.title}](${pr.html_url}) にコメントが追加されました`,
-      `リポジトリ: ${payload.repository.name}`,
-      `追加した人: ${context.actor}`
+      `**リポジトリ**: ${payload.repository.name}`,
+      `**追加した人**: ${context.actor}`
     ].join('\n')
   }
   return content
@@ -69,10 +70,10 @@ const makeMessage = context => {
 try {
   const id = core.getInput('webhook-id')
   const secret = core.getInput('webhook-secret', { required: false })
-  // const content = core.getInput('content')
   const channelId = core.getInput('channel-id', { required: false })
 
   const context = github.context
+  context.payload.comment
   const content = makeMessage(context)
 
   const url = `https://q.trap.jp/api/v3/webhooks/${id}`
