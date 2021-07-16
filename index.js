73d49833dfbf9b8ce993de3aa89f81ddca1ae46b
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-07-16 19:12:56 +0900

    ♻️ separate files & chg variable name

--- index.js
+++ index.js
@@ -2,109 +2,9 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const crypto = require('crypto')
 const fetch = require('node-fetch')
+const makeMessage = require('./utils/makeMessage')
 
 const calcHMACSHA1 = (message, secret) => crypto.createHmac('sha1', secret).update(message).digest('hex')
-
-const makeMessage = context => {
-  let content = null
-  const payload = context.payload
-  core.debug(JSON.stringify(context, null, 2))
-  core.debug("------------------------- ")
-  core.debug(JSON.stringify(payload, null, 2))
-  if (context.eventName === 'issues' && payload.action === 'opened') {
-    const issue = payload.issue
-    content = [
-      `## :git_issue_opened: [${issue.title}](${issue.html_url})が作成されました`,
-      `**リポジトリ**: ${payload.repository.name}`,
-      `**作成者**: ${context.actor}`,
-      ...(issue.body.length === 0 ? [] : ['', '---', issue.body])
-    ].join('\n')
-  } else if (context.eventName === 'issues' && payload.action === 'closed') {
-    const issue = payload.issue
-    content = [
-      `## :git_issue_closed: [${issue.title}](${issue.html_url})が閉じられました`,
-      `**リポジトリ**: ${payload.repository.name}`,
-      `**作成者**: ${context.actor}`,
-      ...(issue.body.length === 0 ? [] : ['', '---', issue.body])
-    ].join('\n')
-  } else if (context.eventName === 'issue_comment' && payload.action === 'created') {
-    const issue = payload.issue
-    if (!('pull_request' in issue)) {
-      content = [
-        `## :comment: issue [${issue.title}](${issue.html_url}) にコメントが追加されました`,
-        `**リポジトリ**: ${payload.repository.name}`,
-        `**コメントした人**: ${context.actor}`,
-        ...(payload.comment.body.length === 0 ? [] : ['', '---', payload.comment.body])
-      ].join('\n')
-    } else {
-      content = [
-        `## :blobenjoy: PR[${issue.title}](${issue.html_url}) にコメントが追加されました`,
-        `**リポジトリ**: ${payload.repository.name}`,
-        `**コメントした人**: ${context.actor}`,
-        ...(payload.comment.body.length === 0 ? [] : ['', '---', payload.comment.body])
-      ].join('\n')
-    }
-  } else if (context.eventName === 'pull_request' && payload.action === 'opened') {
-    const pr = payload.pull_request
-    console.log('is pr')
-    console.log(pr)
-    content = [
-      `## :git_pull_request: [${pr.title}](${pr.html_url}) が作成されました`,
-      `**リポジトリ**: ${payload.repository.name}`,
-      `**作成者**: ${context.actor}`,
-      ...(pr.body.length === 0 ? []: ['', '---', pr.body])
-    ].join('\n')
-  } else if (context.eventName === 'pull_request' && payload.action === 'closed') {
-    const pr = payload.pull_request
-    if (pr.merged) {
-      content = [
-        `## :git_merged: [${pr.title}](${pr.html_url}) がマージされました :tada:`,
-        `**リポジトリ**: ${payload.repository.name}`,
-        `**作成者**: ${context.actor}`,
-        ...(pr.body.length === 0 ? [] : ['', '---', pr.body])
-      ].join('\n')
-    } else {
-      content = [
-        `## :git_pull_request_closed: [${pr.title}](${pr.html_url}) が閉じられました`,
-        `**リポジトリ**: ${payload.repository.name}`,
-        `**作成者**: ${context.actor}`,
-        ...(pr.body.length === 0 ? [] : ['', '---', pr.body])
-      ].join('\n')
-    }
-  } else if (context.eventName === 'pull_request' && payload.action === 'review_requested') {
-    const pr = payload.pull_request
-    content = [
-      `## :blob_bongo: PR[${pr.title}](${pr.html_url}) でレビューがリクエストされました`,
-      `**リポジトリ**: ${payload.repository.name}`,
-      `**リクエストされた人**: ${payload.requested_reviewer.login}`
-    ].join('\n')
-  } else if (context.eventName === 'pull_request_review' && payload.action === 'submitted') {
-    const pr = payload.pull_request
-    if (payload.review.state === 'approved') {
-      content = [
-        `## :partyparrot_blob_cat: PR[${pr.title}](${pr.html_url}) が approve されました`,
-        `**リポジトリ**: ${payload.repository.name}`,
-        `**approve した人**: ${payload.review.user.login}`,
-        ...(payload.review.body.length === 0 ? [] : ['', '---', payload.review.body])
-      ].join('\n')
-    } else if (payload.review.state === 'changes_requested') {
-      content = [
-        `## :Hyperblob: PR[${pr.title}](${pr.html_url}) で変更がリクエストされました`,
-        `**リポジトリ**: ${payload.repository.name}`,
-        `**リクエストした人**: ${payload.review.user.login}`,
-        ...(payload.review.body.length === 0 ? [] : ['', '---', payload.review.body])
-      ].join('\n')
-    } else {
-      content = [
-        `## :blobwobwork: PR[${pr.title}](${pr.html_url}) にレビューコメントが追加されました`,
-        `**リポジトリ**: ${payload.repository.name}`,
-        `**コメントした人**: ${payload.review.user.login}`,
-        ...(payload.review.body === null || payload.review.body.length === 0 ? [] : ['', '---', payload.review.body])
-      ].join('\n')
-    }
-  }
-  return content
-}
 
 try {
   const id = core.getInput('webhook-id')
@@ -123,7 +23,6 @@ try {
     if (channelId !== '-1') {
       headers['X-TRAQ-Channel-Id'] = channelId
     }
-    console.log(headers)
     fetch(url, {
       method: 'POST',
       body: content,
