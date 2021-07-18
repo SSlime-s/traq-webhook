63915d3a713e929575f55ff1add9eb335cef5e0e
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-07-19 01:10:19 +0900

    📚 version up to v4.5

--- dist/index.js
+++ dist/index.js
@@ -6147,30 +6147,30 @@ const makeMessage = (core, context) => {
     const issue = payload.issue
     if (payload.action === 'opened') {
       message = [
-        `## :git_issue_opened: ${createIssueLink(issue)}が作成されました`,
-        `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-        `**作成者**: ${createUserLink(issue.user, false)}`,
+        `## :git_issue_opened: ${createIssueLink(issue, true)}が作成されました`,
+        `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+        `**作成者**: ${createUserLink(issue.user)}`,
         ...(issue.body.length === 0 ? [] : ['', '---', issue.body])
       ].join('\n')
     } else if (payload.action === 'edited') {
       message = [
-        `## :pencil: issue ${createIssueLink(issue)}が編集されました`,
-        `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-        `**編集者**: ${createUserLink(issue.user, false)}`,
+        `## :pencil: issue ${createIssueLink(issue, true)}が編集されました`,
+        `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+        `**編集者**: ${createUserLink(issue.user)}`,
         ...(issue.body.length === 0 ? [] : ['', '---', issue.body])
       ].join('\n')
     } else if (payload.action === 'closed') {
       message = [
-        `## :git_issue_closed: ${createIssueLink(issue)}が閉じられました`,
-        `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-        `**閉じた人**: ${createUserLink(issue.user, false)}`,
+        `## :git_issue_closed: ${createIssueLink(issue, true)}が閉じられました`,
+        `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+        `**閉じた人**: ${createUserLink(issue.user)}`,
         ...(issue.body.length === 0 ? [] : ['', '---', issue.body])
       ].join('\n')
     } else if (payload.action === 'reopened') {
       message = [
-        `## :git_issue_opened: ${createIssueLink(issue)}が再び開かれました`,
-        `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-        `**開いた人**: ${createUserLink(issue.user, false)}`,
+        `## :git_issue_opened: ${createIssueLink(issue, true)}が再び開かれました`,
+        `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+        `**開いた人**: ${createUserLink(issue.user)}`,
         ...(issue.body.length === 0 ? [] : ['', '---', issue.body])
       ].join('\n')
     }
@@ -6182,32 +6182,32 @@ const makeMessage = (core, context) => {
     if ('pull_request' in issue) {
       if (payload.action === 'created') {
         message = [
-          `## :blobenjoy: PR ${createIssueLink(issue)} にコメントが追加されました`,
-          `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-          `**コメントした人**: ${createUserLink(issue.user, false)}`,
+          `## :blobenjoy: PR ${createIssueLink(issue, true)} にコメントが追加されました`,
+          `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+          `**コメントした人**: ${createUserLink(issue.user)}`,
           ...(payload.comment.body.length === 0 ? [] : ['', '---', payload.comment.body])
         ].join('\n')
       } else if (payload.action === 'edited') {
         message = [
-          `## :blobenjoy: PR ${createIssueLink(issue)} のコメントが編集されました`,
-          `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-          `**編集者**: ${createUserLink(issue.user, false)}`,
+          `## :blobenjoy: PR ${createIssueLink(issue, true)} のコメントが編集されました`,
+          `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+          `**編集者**: ${createUserLink(issue.user)}`,
           ...(payload.comment.body.length === 0 ? [] : ['', '---', payload.comment.body])
         ].join('\n')
       }
     } else {
       if (payload.action === 'created') {
         message = [
-          `## :comment: issue ${createIssueLink(issue)} にコメントが追加されました`,
-          `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-          `**コメントした人**: ${createUserLink(issue.user, false)}`,
+          `## :comment: issue ${createIssueLink(issue, true)} にコメントが追加されました`,
+          `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+          `**コメントした人**: ${createUserLink(issue.user)}`,
           ...(payload.comment.body.length === 0 ? [] : ['', '---', payload.comment.body])
         ].join('\n')
       } else if (payload.action === 'edited') {
         message = [
-          `## :comment: issue ${createIssueLink(issue)} のコメントが編集されました`,
-          `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-          `**編集者**: ${createUserLink(issue.user, false)}`,
+          `## :comment: issue ${createIssueLink(issue, true)} のコメントが編集されました`,
+          `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+          `**編集者**: ${createUserLink(issue.user)}`,
           ...(payload.comment.body.length === 0 ? [] : ['', '---', payload.comment.body])
         ].join('\n')
       }
@@ -6218,39 +6218,39 @@ const makeMessage = (core, context) => {
     const pr = payload.pull_request
     if (payload.action === 'opened') {
       message = [
-        `## :git_pull_request: PR ${createIssueLink(pr)} が作成されました`,
-        `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-        `**作成者**: ${createUserLink(payload.user, false)}`,
+        `## :git_pull_request: PR ${createIssueLink(pr, true)} が作成されました`,
+        `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+        `**作成者**: ${createUserLink(payload.user)}`,
         ...(pr.body.length === 0 ? []: ['', '---', pr.body])
       ].join('\n')
     } else if (body.action === 'edited') {
       message = [
-        `## :git_pull_request: PR ${createIssueLink(pr)} が編集されました`,
-        `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-        `**編集者**: ${createUserLink(payload.user, false)}`,
+        `## :git_pull_request: PR ${createIssueLink(pr, true)} が編集されました`,
+        `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+        `**編集者**: ${createUserLink(payload.user)}`,
         ...(pr.body.length === 0 ? []: ['', '---', pr.body])
       ].join('\n')
     } else if (body.action === 'closed') {
       if (pr.merged) {
         message = [
-          `## :git_merged: PR ${createIssueLink(pr)} がマージされました :tada:`,
-          `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-          `**マージした人**: ${createUserLink(payload.user, false)}`,
+          `## :git_merged: PR ${createIssueLink(pr, true)} がマージされました :tada:`,
+          `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+          `**マージした人**: ${createUserLink(payload.user)}`,
           ...(pr.body.length === 0 ? [] : ['', '---', pr.body])
         ].join('\n')
       } else {
         message = [
-          `## :git_pull_request_closed: PR ${createIssueLink(pr)} が閉じられました`,
-          `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-          `**閉じた人**: ${createUserLink(payload.user, false)}`,
+          `## :git_pull_request_closed: PR ${createIssueLink(pr, true)} が閉じられました`,
+          `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+          `**閉じた人**: ${createUserLink(payload.user)}`,
           ...(pr.body.length === 0 ? [] : ['', '---', pr.body])
         ].join('\n')
       }
     } else if (payload.action === 'review_requested') {
       message = [
-        `## :blob_bongo: PR ${createIssueLink(pr)} でレビューがリクエストされました`,
-        `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-        `**リクエストされた人**: ${createdUserLink(payload.requested_reviewer, false)}`
+        `## :blob_bongo: PR ${createIssueLink(pr, true)} でレビューがリクエストされました`,
+        `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+        `**リクエストされた人**: ${createdUserLink(payload.requested_reviewer)}`
       ].join('\n')
     }
   }
@@ -6260,23 +6260,23 @@ const makeMessage = (core, context) => {
     if (payload.action === 'submitted') {
       if (payload.review.state === 'approved') {
         message = [
-          `## :partyparrot_blob_cat: PR ${createIssueLink(pr)} が approve されました`,
-          `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-          `**approve した人**: ${craeteUserLink(payload.review.user, false)}`,
+          `## :partyparrot_blob_cat: PR ${createIssueLink(pr, true)} が approve されました`,
+          `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+          `**approve した人**: ${createUserLink(payload.review.user)}`,
           ...(payload.review.body.length === 0 ? [] : ['', '---', payload.review.body])
         ].join('\n')
       } else if (payload.review.state === 'changes_requested') {
         message = [
-          `## :Hyperblob: PR ${createIssueLink(pr)} で変更がリクエストされました`,
-          `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-          `**リクエストした人**: ${craeteUserLink(payload.review.user, false)}`,
+          `## :Hyperblob: PR ${createIssueLink(pr, true)} で変更がリクエストされました`,
+          `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+          `**リクエストした人**: ${createUserLink(payload.review.user)}`,
           ...(payload.review.body.length === 0 ? [] : ['', '---', payload.review.body])
         ].join('\n')
       } else {
         message = [
-          `## :blobwobwork: PR ${createIssueLink(pr)} にレビューコメントが追加されました`,
-          `**リポジトリ**: ${createRepoLink(payload.repository, false)}`,
-          `**コメントした人**: ${craeteUserLink(payload.review.user, false)}`,
+          `## :blobwobwork: PR ${createIssueLink(pr, true)} にレビューコメントが追加されました`,
+          `**リポジトリ**: ${createRepoLink(payload.repository)}`,
+          `**コメントした人**: ${createUserLink(payload.review.user)}`,
           ...(payload.review.body === null || payload.review.body.length === 0 ? [] : ['', '---', payload.review.body])
         ].join('\n')
       }
