7a75954a807ca32c99673baf86bca54ca22fc6de
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2024-12-21 15:51:06 +0900

    ✨ デプロイスクリプト更新

--- bin/deploy.ts
+++ bin/deploy.ts
@@ -1,141 +1,159 @@
 #!/usr/bin/env -S deno run --allow-run --allow-read --allow-write
-// deno-lint-ignore-file no-deprecated-deno-api
+
+/**
+ * USAGE:
+ * 		./bin/deploy.ts [major|minor|patch]
+ *
+ * DESCRIPTION:
+ * 		- バージョンアップを行うスクリプト
+ * 		- バージョンアップの種類を指定しない場合は、
+ * 		  最新のタグが patch バージョンを持っている場合は patch、
+ * 		  そうでない場合は minor バージョンアップを行う
+ */
 
 import * as colors from "https://deno.land/std@0.203.0/fmt/colors.ts";
 import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
+import $ from 'jsr:@david/dax@0.42.0'
 
 const versionUpSchema = z.enum(["major", "minor", "patch"]);
 type VersionUp = z.infer<typeof versionUpSchema>;
 
-const getOutput = async (cmd: Deno.Process): Promise<string> => {
-	const { code } = await cmd.status();
-	if (code !== 0) {
-		throw new Error("command failed");
-	}
+/**
+ * package.json からバージョンを取得する
+ */
+async function getLatestVersion(): Promise<`${number}.${number}.${number}`> {
+	const content = await Deno.readTextFile("package.json");
+	const json = JSON.parse(content);
+	return json.version;
+}
 
-	const rawOutput = await cmd.output();
-	const output = new TextDecoder().decode(rawOutput);
-	return output;
-};
-
-const getLatestTag = async () => {
-	const p = Deno.run({
-		cmd: ["git", "describe", "--abbrev=0", "--tags"],
-		stdout: "piped",
-	});
-
-	const output = await getOutput(p);
-	if (!output.startsWith("v")) {
-		console.error(colors.red("[ERROR]"), "invalid tag:", output);
-		Deno.exit(1);
-	}
-	return output.trim();
-};
-
+/**
+ * バージョンを表す構造体
+ */
 type Version = {
 	major: number;
 	minor: number;
-	patch: number | undefined;
+	patch: number;
 };
-const stringifyVersion = (version: Version): string => {
-	const { major, minor, patch } = version;
-	const concat = [major, minor, patch].filter((v) => v !== undefined);
-	return concat.join(".");
-};
+type VersionString = `${number}.${number}.${number}`;
+type VersionTagString = `v${VersionString}`;
+/**
+ * Version 構造体を major.minor.patch の形式の文字列に変換する
+ */
+function stringifyVersion(version: Version): VersionString {
+  const { major, minor, patch } = version;
+  return `${major}.${minor}.${patch}`;
+}
 
-const parseVersion = (version: string): Version => {
-	const [major, minor, patch] = version
-		.split(".")
-		.map((v) => Number(v))
-		.map((v) => (Number.isNaN(v) ? undefined : v));
+/**
+ * major.minor.patch の形式の文字列を Version 構造体に変換する
+ */
+function parseVersion(version: VersionString): Version {
+  const [major, minor, patch] = version
+    .split(".")
+    .map((v) => Number(v))
+    .map((v) => (Number.isNaN(v) ? undefined : v));
 
-	if (major === undefined || minor === undefined) {
-		console.error(colors.red("[ERROR]"), "invalid version:", version);
-		Deno.exit(1);
+  if (major === undefined || minor === undefined || patch === undefined) {
+    console.error(colors.red("[ERROR]"), "invalid version:", version);
+    Deno.exit(1);
+  }
+
+  return { major, minor, patch };
+}
+
+/**
+ * versionUp で指定されたバージョンアップをした構造体を返す
+ */
+function bumpVersion(version: Version, versionUp: VersionUp): Version {
+  const { major, minor, patch } = version;
+
+  switch (versionUp) {
+    case "major":
+      return { major: major + 1, minor: 0, patch: 0 };
+    case "minor":
+      return { major, minor: minor + 1, patch: 0 };
+    case "patch": {
+      return { major, minor, patch: patch + 1 };
+    }
+  }
+}
+
+/**
+ * git diff で dist 以外に変更があった場合はエラーを出力して終了する
+ */
+async function checkDiff() {
+	const output = await $`git diff --name-only HEAD -- ":(exclude)dist/*"`.text()
+  if (output.trim() === "") {
+    return;
 	}
 
-	return { major, minor, patch };
-};
+  console.error(colors.red("[ERROR]"), "diff found:\n", output);
+  Deno.exit(1);
+}
 
-const bumpVersion = (version: Version, versionUp: VersionUp): Version => {
-	const { major, minor, patch } = version;
+/**
+ * v*.*.* の形式のタグをパースして Version 構造体に変換する
+ */
+function parseTag(tag: VersionTagString): Version {
+  if (!tag.startsWith("v")) {
+    console.error(colors.red("[ERROR]"), "invalid tag:", tag);
+    Deno.exit(1);
+  }
 
-	switch (versionUp) {
-		case "major":
-			return { major: major + 1, minor: 0, patch };
-		case "minor":
-			return { major, minor: minor + 1, patch };
-		case "patch": {
-			if (patch === undefined) {
-				console.error(colors.red("[ERROR]"), "invalid keyword `patch`");
-				Deno.exit(1);
-			}
+  return parseVersion(tag.replace(/^v/, "") as VersionString);
+}
+/**
+ * Version 構造体を v*.*.* の形式の文字列に変換する
+ */
+function stringifyTag(version: Version): VersionTagString {
+  return `v${stringifyVersion(version)}`;
+}
 
-			return { major, minor, patch: patch + 1 };
-		}
-	}
-};
+/**
+ * npm run bundle で dist ディレクトリを更新する
+ */
+async function runBundle() {
+	await $`npm run bundle`
+}
 
-const checkDiff = async () => {
-	const p = Deno.run({
-		cmd: ["git", "diff", "--name-only", "HEAD", "--", ":(exclude)dist/*"],
-		stdout: "piped",
-	});
+/**
+ * package.json の version を newVersion に更新する
+ */
+async function updatePackageJson(newVersion: Version) {
+  const content = await Deno.readTextFile("package.json");
+  const json = JSON.parse(content);
+  json.version = stringifyVersion(newVersion);
+  await Deno.writeTextFile("package.json", JSON.stringify(json, null, 2));
+}
 
-	const output = await getOutput(p);
-	if (output.trim() === "") {
-		return;
-	}
+/**
+ * README.md 内の @v1 のように指定されたバージョンを更新する
+ *
+ * README.md には major バージョンでの指定しか書かない
+ */
+async function updateReadme(newVersion: Version) {
+  // like SSlime-s/traq-webhook@v1 format
+  const content = await Deno.readTextFile("README.md");
+  const regex = /SSlime-s\/traq-webhook@v\d+/g;
 
-	console.error(colors.red("[ERROR]"), "diff found:\n", output);
-	Deno.exit(1);
-};
+  const newContent = content.replace(
+    regex,
+    `SSlime-s/traq-webhook@v${newVersion.major}`,
+  );
+  await Deno.writeTextFile("README.md", newContent);
+}
 
-const parseTag = (tag: string): Version => {
-	if (!tag.startsWith("v")) {
-		console.error(colors.red("[ERROR]"), "invalid tag:", tag);
-		Deno.exit(1);
-	}
-
-	return parseVersion(tag.slice(1));
-};
-const stringifyTag = (version: Version): string => {
-	return `v${stringifyVersion(version)}`;
-};
-
-const runBundle = async () => {
-	const p = Deno.run({
-		cmd: ["npm", "run", "bundle"],
-		stdout: "piped",
-	});
-
-	await getOutput(p);
-};
-
-const updatePackageJson = async (newVersion: Version) => {
-	const content = await Deno.readTextFile("package.json");
-	const json = JSON.parse(content);
-	json.version = stringifyVersion(newVersion);
-	await Deno.writeTextFile("package.json", JSON.stringify(json, null, 2));
-};
-const updateReadme = async (newVersion: Version) => {
-	// like SSlime-s/traq-webhook@v1.2 format
-	const content = await Deno.readTextFile("README.md");
-	const regex = /SSlime-s\/traq-webhook@v\d+(?:\.\d+)+/g;
-
-	const newContent = content.replace(
-		regex,
-		`SSlime-s/traq-webhook@${stringifyTag(newVersion)}`,
-	);
-	await Deno.writeTextFile("README.md", newContent);
-};
+// ---------------------------------
+// ここまで関数定義
+// ---------------------------------
 
 await checkDiff();
 
 const args = Deno.args;
 
-const latestTag = await getLatestTag();
-const parsedLatestTag = parseTag(latestTag);
+const latestVersion = await getLatestVersion();
+const parsedLatestTag = parseVersion(latestVersion);
 
 let versionUp: VersionUp;
 if (args.length > 0) {
@@ -147,11 +165,8 @@ if (args.length > 0) {
 
 	versionUp = res.data;
 } else {
-	if (parsedLatestTag.patch !== undefined) {
-		versionUp = "patch";
-	} else {
-		versionUp = "minor";
-	}
+	console.error(colors.yellow("[ERROR]"), "version up type is not specified");
+	Deno.exit(1);
 }
 
 const newVersion = bumpVersion(parsedLatestTag, versionUp);
@@ -167,41 +182,15 @@ await runBundle();
 await updatePackageJson(newVersion);
 await updateReadme(newVersion);
 
-{
-	const p = Deno.run({
-		cmd: ["git", "add", "README.md", "package.json", "dist"],
-	});
-	await p.status();
-}
+await $`git add README.md package.json dist`
 
 const newTag = stringifyTag(newVersion);
 
 const commitMessage = `🎉 bump version (${newTag})`;
 
-{
-	const p = Deno.run({
-		cmd: ["git", "commit", "-m", commitMessage],
-	});
-	await p.status();
-}
+await $`git commit -m ${commitMessage}`
 
-{
-	const p = Deno.run({
-		cmd: ["git", "tag", newTag],
-	});
-	await p.status();
-}
+await $`git tag ${newTag}`
 
-{
-	const p = Deno.run({
-		cmd: ["git", "push"],
-	});
-	await p.status();
-}
-
-{
-	const p = Deno.run({
-		cmd: ["git", "push", "--tags"],
-	});
-	await p.status();
-}
+await $`git push`
+await $`git push --tags`
